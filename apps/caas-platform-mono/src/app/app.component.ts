e528de9bb7623674206fa3ccb0e5d1c96d663824
Sebastian Copei <sco@uni-kassel.de>
2020-12-21 09:46:28 +0100

    initial commit

---
+++ apps/caas-platform-mono/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'caas-platform-mono-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'caas-platform-mono';
+}
