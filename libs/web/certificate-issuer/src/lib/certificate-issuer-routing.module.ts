fd56393b4dc93fa36c7edbbbe2163308529db270
Julian Holfeld <julianholfeld@student.uni-kassel.de>
2021-03-31 13:46:17 +0200

    feat: Added certificate-overview-component

--- libs/web/certificate-issuer/src/lib/certificate-issuer-routing.module.ts
+++ libs/web/certificate-issuer/src/lib/certificate-issuer-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CertificateComponent } from './certificate.component';
+import { CertificateOverviewComponent } from './overview';
 
 const routes: Routes = [
   {
     path: '',
     component: CertificateComponent,
+    children: [
+      { path: 'overview', component: CertificateOverviewComponent },
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
+    ],
   },
 ];
 
